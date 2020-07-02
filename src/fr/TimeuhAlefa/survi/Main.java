fbe890b1c9651acdc6bd66976ae7ad79496956af
Miuby <miluby@live.fr>
2020-07-02 11:13:58 +0200

    Base

---
+++ src/fr/TimeuhAlefa/survi/Main.java
@@ -0,0 +1,15 @@
+package fr.TimeuhAlefa.survi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
