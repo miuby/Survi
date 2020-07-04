6d909faa5bedfd4d7505d32bcdb4593e7eaa19d5
Miuby <miluby@live.fr>
2020-07-04 16:57:11 +0200

    Ajout du world

--- src/fr/TimeuhAlefa/survi/Main.java
+++ src/fr/TimeuhAlefa/survi/Main.java
@@ -4,24 +4,23 @@ import fr.TimeuhAlefa.survi.listener.MyListener;
 import fr.TimeuhAlefa.survi.listener.PlayerListener;
 import fr.TimeuhAlefa.survi.listener.ServerListener;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
 public class Main extends JavaPlugin {
+    public final GameManager gameManager = new GameManager(this);
+    public final Timer timer = new Timer(this);
 
     private final MyListener listener = new MyListener(this);
     private final PlayerListener playerListener = new PlayerListener(this);
     private final ServerListener serverListener = new ServerListener(this);
-
     private final Commands commands = new Commands(this);
-    public final GameManager gameManager = new GameManager(this);
-    public final Timer timer = new Timer(this);
-    Objective objectiveHealth;
+    private World world;
 
     final Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
 
@@ -35,8 +34,7 @@ public class Main extends JavaPlugin {
         getConfig().options().copyDefaults(true);
         saveConfig();
 
-        /*objectiveHealth = board.registerNewObjective("Health", Criterias.HEALTH);
-        objectiveHealth.setDisplaySlot(DisplaySlot.PLAYER_LIST);*/
+        world = Bukkit.getServer().getWorlds().get(0);
 
         for(Player player : Bukkit.getOnlinePlayers())
         {
